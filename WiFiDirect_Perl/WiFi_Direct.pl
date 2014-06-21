107683dfb93872e4a3efb7760a581d58949edb68
Anvesh Yalamarthy <anvyal@gmail.com>
2014-06-21 17:38:45 +0530

    Integrated HTTP Server

--- WiFiDirect_Perl/WiFi_Direct.pl
+++ WiFiDirect_Perl/WiFi_Direct.pl
@@ -12,13 +12,15 @@ devices::openWifiDirect($device1);
 devices::openWifiDirect($device2);
 
 #Get P2P Device ID of DUTs
-
 $P2P_Device1 = `adb -s $device1 shell getprop P2PdeviceID`;
 $P2P_Device2 = `adb -s $device2 shell getprop P2PdeviceID`;
 
 #devices::searchDevices($device1);
 #devices::searchDevices($device2);
 
-devices::sendInvite($device2, $P2P_Device1);
+devices::sendInvite( $device2, $P2P_Device1 );
 
 devices::acceptInvite($device1);
+
+$ip1 = devices::startServer($device1);
+$ip2 = devices::startServer($device2);
---
+++ WiFiDirect_UIAutomator/src/com/qualcomm/httpserver/startServer.java
@@ -0,0 +1,50 @@
+package com.qualcomm.httpserver;
+
+import android.util.Log;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class startServer extends UiAutomatorTestCase {
+
+	private static final String LOG_TAG = "WiFiDirectAutomation";
+
+	public void testDemo() {
+		UiObject stopServer = new UiObject(new UiSelector().className("android.widget.ToggleButton").text("Stop server"));
+		try {
+			if (stopServer.isEnabled() == true) {
+				Log.e(LOG_TAG, "KWS HTTP Server already started");
+
+				UiObject serverLog = new UiObject(new UiSelector().className("android.widget.EditText").index(1));
+				String temp = serverLog.getText();
+				System.out.println(temp);
+				//new print2File(temp);
+			}
+
+		} catch (UiObjectNotFoundException e) {
+			UiObject startServer = new UiObject(new UiSelector().className("android.widget.ToggleButton").text("Start server"));
+			try {
+				Log.e(LOG_TAG, "Starting KWS HTTP Server");
+				startServer.clickAndWaitForNewWindow();
+				UiObject serverLog = new UiObject(new UiSelector().className("android.widget.EditText").index(1));
+				String temp = serverLog.getText();
+				System.out.println(temp);
+			} catch (UiObjectNotFoundException e1) {
+				Log.e(LOG_TAG, "Unable to KWS HTTP Start Server");
+			}
+		}
+	}
+
+	public String getProperty(String propName) {
+		String propValue = null;
+		try {
+			propValue = (String) Class.forName("android.os.SystemProperties").getMethod("get", new Class[] { String.class }).invoke(null, propName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return propValue;
+	}
+}
