8930b6e6330ea145453b0cdfc6aef06ce22af133
Anvesh Yalamarthy <anvyal@gmail.com>
2014-07-14 18:03:03 +0530

    Handling PIN Sent Dialogue box.. PIN recieve pending

--- WiFiDirect_UIAutomator/Run_jar.bat
+++ WiFiDirect_UIAutomator/Run_jar.bat
@@ -1,5 +1,5 @@
 rem ant build
 cd bin
-adb -s 1420e50 push WiFiDirect_UIAutomator.jar /data/local/tmp/
-adb -s 1420e50 shell uiautomator runtest WiFiDirect_UIAutomator.jar -c com.qualcomm.wifidirect.searchDevices
+adb -s 141ff73 push WiFiDirect_UIAutomator.jar /data/local/tmp/
+adb -s 141ff73 shell uiautomator runtest WiFiDirect_UIAutomator.jar -c com.qualcomm.wifidirect.sendInvite
 cd ..
--- WiFiDirect_UIAutomator/src/com/qualcomm/wifidirect/sendInvite.java
+++ WiFiDirect_UIAutomator/src/com/qualcomm/wifidirect/sendInvite.java
@@ -1,23 +1,36 @@
 package com.qualcomm.wifidirect;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import android.util.Log;
 
+import com.android.uiautomator.core.UiDevice;
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.core.UiWatcher;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class sendInvite extends UiAutomatorTestCase {
 
 	private static final String LOG_TAG = "WiFiDirectAutomation";
+	private static final String MYOKCANCELDIALOGWATCHER_STRING = "InvitationWatcher";
 
 	public void testDemo() throws UiObjectNotFoundException, FileNotFoundException {
 
 		String peerID = getProperty("PeerID");
 		int success = 0;
+
+		// set P2Ppin to NA
+		try {
+			String progArray = "setprop P2Ppin NA";
+			java.lang.Process p = Runtime.getRuntime().exec(progArray);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		for (int i = 1; i <= 3 && success == 0; i++) {
 			System.out.println("Trying to Send Invitation for " + i + " time");
 			success = connect(peerID);
@@ -73,6 +86,8 @@ public class sendInvite extends UiAutomatorTestCase {
 				}
 			}
 		}
+		sleep(2000);
+		invitePIN();
 		return success;
 	}
 
@@ -85,5 +100,59 @@ public class sendInvite extends UiAutomatorTestCase {
 			e.printStackTrace();
 		}
 		return propValue;
+	}
+
+	public void invitePIN() {
+
+		// Define watcher
+		UiWatcher InvitationWatcher = new UiWatcher() {
+			@Override
+			public boolean checkForCondition() {
+				UiObject okCancelDialog = new UiObject(new UiSelector().textContains("Invitation sent"));
+				UiObject pin;
+				String pinID = null;
+
+				if (okCancelDialog.exists()) {
+					Log.w(LOG_TAG, "Sending PIN Invitation !");
+					UiObject pinParent = new UiObject(new UiSelector().className("android.widget.TextView").text("PIN:"));
+					try {
+						pin = pinParent.getFromParent(new UiSelector().className("android.widget.TextView").index(1));
+						pinID = pin.getText();
+					} catch (UiObjectNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+					System.out.println("Found Box and PIN ID is " + pinID);
+
+					UiObject okButton = new UiObject(new UiSelector().className("android.widget.Button").text("OK"));
+					try {
+						okButton.click();
+						try {
+							String progArray = "setprop P2Ppin " + pinID;
+							java.lang.Process p = Runtime.getRuntime().exec(progArray);
+						}
+
+						catch (IOException e) {
+							e.printStackTrace();
+						}
+					} catch (UiObjectNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					return (okCancelDialog.waitUntilGone(25000));
+				}
+				return false;
+			}
+		};
+
+		// Register watcher
+		UiDevice.getInstance().registerWatcher(MYOKCANCELDIALOGWATCHER_STRING, InvitationWatcher);
+
+		// Run watcher
+		sleep(1000);
+		UiDevice.getInstance().runWatchers();
+		// getUiDevice().pressBack();
 	}
 }
